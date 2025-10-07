97e948d24d9bdab519ac194d6d3f9e1e58af9124
unknown <listen.veigas22@gmail.com>
2025-10-07 22:08:56 +0530

    index file created

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import express from 'express'
+import userRoutes from "./routes/userRoutes.js"
+import llmRoutes from "./routes/llmRoutes.js"
 
-console.log('Hello from ts');
+const app = express();
+
+app.use(express.json());
+
+app.use("/users",userRoutes);
+app.use("/llmResponse",llmRoutes)
+
+const PORT = 8000;
+app.listen(PORT,()=>{
+    console.log(`Server running n http://localhost:${PORT}`)
+})
